3bfab077d63b4fa18fa9c24fe5a6a4a7c93d1479
dimitri vandevelde <vandevelde.dimitri@hotmail.com>
2024-08-03 13:02:36 +0200

    change style and add new feature

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-const result = document.querySelector("#result");
-const submit = document.querySelector("#submit");
+document.querySelectorAll(".form").forEach((form) => {
+  const result = form.querySelector(".result p");
+  const submit = form.querySelector("button");
 
-const canUseTruck = () => {
-  const grande_palette = document.querySelector("#gp").value;
-  const demi_palette = document.querySelector("#dp").value;
+  const canUseTruck = () => {
+    const grande_palette = form.querySelector("#gp").value;
+    const demi_palette = form.querySelector("#dp").value;
 
-  const max_palette = 66;
-  const demi_palettePerGrande = 2;
+    const max_palette = 66;
+    const demi_palettePerGrande = 2;
 
-  const total_palette =
-    parseInt(grande_palette) * demi_palettePerGrande + parseInt(demi_palette);
+    const total_demi_palette =
+      parseInt(grande_palette) * demi_palettePerGrande + parseInt(demi_palette);
+    const rest_demi_palette_sucess = max_palette - total_demi_palette;
+    const rest_demi_palette_error = total_demi_palette - max_palette;
 
-  console.log("total_palette", total_palette);
-  console.log("grande_palette", grande_palette);
-  console.log("demi_palette", demi_palette);
+    const palettes = Math.floor(total_demi_palette / demi_palettePerGrande);
+    const demi_palettes = total_demi_palette % demi_palettePerGrande;
 
-  if (total_palette <= max_palette) {
-    result.classList.remove("error");
-    result.classList.add("success");
-    result.innerHTML = "Vous pouvez utiliser le camion";
-  } else {
-    result.classList.remove("success");
-    result.classList.add("error");
-    result.innerHTML = "Vous ne pouvez pas utiliser le camion";
-  }
-};
+    console.log("total_demi_palette", total_demi_palette);
+    console.log("grande_palette", grande_palette);
+    console.log("demi_palette", demi_palette);
 
-submit.addEventListener("click", canUseTruck);
+    if (total_demi_palette <= max_palette) {
+      result.classList.remove("error");
+      result.classList.add("success");
+      result.innerHTML = `Vous pouvez utiliser le camion, il reste ${Math.floor(
+        rest_demi_palette_sucess / demi_palettePerGrande
+      )} palette(s) et ${
+        rest_demi_palette_sucess % demi_palettePerGrande
+      } demi-palette(s) disponible(s).`;
+    } else {
+      result.classList.remove("success");
+      result.classList.add("error");
+      result.innerHTML = `Vous ne pouvez pas utiliser le camion, il y a ${Math.floor(
+        rest_demi_palette_error / demi_palettePerGrande
+      )} palette(s) et ${
+        rest_demi_palette_error % demi_palettePerGrande
+      } demi-palette(s) de trop.`;
+    }
+  };
+
+  submit.addEventListener("click", canUseTruck);
+});
